10706446413721a7fb7066e47273befd474f91d3
SabbirHowlader <sabbirahmedreza56@gmail.com>
2023-02-12 23:29:15 +0600

    added banner

--- src/Page/Home/TopBanner/TopBanner.js
+++ src/Page/Home/TopBanner/TopBanner.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import banner from '../../../Image/TopBanner.jpg'
 
 const TopBanner = () => {
     return (
-        <div className="text-center text-white bg-slate-400 py-24 ">
-            <h3 className="mb-5 text-5xl font-bold">Welcome to <span className='text-orange-300'>Innovation!</span></h3>
-            <p className="mb-5 sm:w-full lg:w-1/2 mx-auto">Ezze Technology Ltd. is one of the most popular Software Company in Bangladesh. Successfully we done lot of web development projects. Our dream is going forward with High and New Technology will bring success in every way. </p>
+        <div className="hero relative overflow-hidden bg-no-repeat min-h-screen" style={{ backgroundImage: `url(${banner})` }} >
+            <div className="hero-overlay bg-opacity-60 "></div>
+
+            <div className="text-center text-white">
+                <h3 className="mb-5 text-5xl font-bold">Welcome to <span className='text-orange-300'>Innovation!</span></h3>
+                <p className="mb-5 sm:w-full lg:w-1/2 mx-auto">Ezze Technology Ltd. is one of the most popular Software Company in Bangladesh. Successfully we done lot of web development projects. Our dream is going forward with High and New Technology will bring success in every way. </p>
+            </div>
+
         </div>
     );
 };
