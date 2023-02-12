a68d74c61e1d5cb07f058afd51a06753fcc20edc
SabbirHowlader <sabbirahmedreza56@gmail.com>
2023-02-12 23:37:40 +0600

    added all

--- src/Page/Home/TopBanner/TopBanner.js
+++ src/Page/Home/TopBanner/TopBanner.js
@@ -7,7 +7,7 @@ const TopBanner = () => {
             <div className="hero-overlay bg-opacity-60 "></div>
 
             <div className="text-center text-white">
-                <h3 className="mb-5 text-5xl font-bold">Welcome to <span className='text-orange-300'>Innovation!</span></h3>
+                <h3 className="mb-5 text-5xl font-bold">Welcome to <span className='text-orange-400'>Innovation!</span></h3>
                 <p className="mb-5 sm:w-full lg:w-1/2 mx-auto">Ezze Technology Ltd. is one of the most popular Software Company in Bangladesh. Successfully we done lot of web development projects. Our dream is going forward with High and New Technology will bring success in every way. </p>
             </div>
 
